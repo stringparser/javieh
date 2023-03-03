3751d56aeea1edfee2b4bfcc21cae8705712aa98
Javier Carrillo Milla <javier.carrillo.milla@extern.wtl.de>
2023-03-03 11:46:38 +0100

    fix index title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,41 @@
 import clsx from 'clsx';
 import React from 'react';
+import {Helmet} from 'react-helmet';
 
 import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 import styles from './index.module.css';
-import { ProfilePicture } from '@site/src/features/profile/ProfilePicture';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero', styles.heroBanner)}>
-      <div className="container">
-        <p className="hero__subtitle">Hey there come in, I'm Javier</p>
-        <p>You can find some stuff I've been up to here like:</p>
-        <div className="spacer"/>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/music">
-            Music
-          </Link>
-          <div style={{padding: '1rem'}} />
-          <Link
-            className="button button--secondary button--lg"
-            to="/blog">
-            Blog
-          </Link>
+    <>
+      <Helmet>
+        <title>Hello there</title>
+      </Helmet>
+      <header className={clsx('hero', styles.heroBanner)}>
+        <div className="container">
+          <p className="hero__subtitle">Hey there come in, I'm Javier</p>
+          <p>You can find some stuff I've been up to here like:</p>
+          <div className="spacer"/>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/music">
+              Music
+            </Link>
+            <div style={{padding: '1rem'}} />
+            <Link
+              className="button button--secondary button--lg"
+              to="/blog">
+              Blog
+            </Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
 
